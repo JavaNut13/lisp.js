f8b258e11a971c75ccd21967d99ca67875cf00a1
Will Richardson <william.hamish@gmail.com>
2016-04-01 10:40:14 +1300

    Object version of generator works

--- generate.js
+++ generate.js
@@ -1,21 +1,20 @@
-var generator = {};
-
-generator.get = function(item) {
-  return generator[item.type](item);
+function Generator() {
 }
 
-generator.getAll = function(items, i) {
+Generator.prototype.get = function(item) {
+  return this[item.type](item);
+}
+
+Generator.prototype.getAll = function(items, i) {
   i = i || 0;
   var res = []
   for(; i < items.length; i++) {
-    res.push(generator.get(items[i]));
+    res.push(this.get(items[i]));
   }
   return res;
 }
 
-generator.builtins = {}
-
-generator.infix_operators = {
+Generator.prototype.infix_operators = {
   '+': true,
   '-': true,
   '/': true,
@@ -29,18 +28,19 @@ generator.infix_operators = {
   '>=': true,
   '=': '==='
 }
-generator.infix = function(obj) {
-  var operator = generator.get(obj.cont[0]);
-  if(generator.infix_operators[operator] !== true) {
-    operator = generator.infix_operators[operator];
+
+Generator.prototype.infix = function(obj) {
+  var operator = this.get(obj.cont[0]);
+  if(this.infix_operators[operator] !== true) {
+    operator = this.infix_operators[operator];
   }
-  var args = generator.getAll(obj.cont.slice(1));
+  var args = this.getAll(obj.cont.slice(1));
   return '(' + args.join(operator) + ')';
 }
 
-generator.builtins.fn = function(items) {
-  var args = generator.getAll(items[0].cont.cont);
-  var statements = generator.getAll(items.slice(1));
+Generator.prototype.builtins_fn = function(items) {
+  var args = this.getAll(items[0].cont.cont);
+  var statements = this.getAll(items.slice(1));
   var last = statements[statements.length - 1];
   statements[statements.length - 1] = 'return ' + last;
   var code = statements.join(';\n');
@@ -50,10 +50,10 @@ generator.builtins.fn = function(items) {
   return res;
 }
 
-generator.builtins.defn = function(items) {
-  var funcName = generator.get(items[0]);
-  var args = generator.getAll(items[1].cont.cont);
-  var statements = generator.getAll(items.slice(2));
+Generator.prototype.builtins_defn = function(items) {
+  var funcName = this.get(items[0]);
+  var args = this.getAll(items[1].cont.cont);
+  var statements = this.getAll(items.slice(2));
   var last = statements[statements.length - 1];
   statements[statements.length - 1] = 'return ' + last;
   var code = statements.join(';\n');
@@ -63,63 +63,63 @@ generator.builtins.defn = function(items) {
   return res;
 }
 
-generator.builtins.def = function(items) {
-  return 'var ' + generator.get(items[0]) + '=' + generator.get(items[1]);
+Generator.prototype.builtins_def = function(items) {
+  return 'var ' + this.get(items[0]) + '=' + this.get(items[1]);
 }
 
-generator.builtins.if = function(items) {
-  var condition = generator.get(items[0]);
-  var ifState = generator.get(items[1]);
-  var elseState = generator.get(items[2]);
+Generator.prototype.builtins_if = function(items) {
+  var condition = this.get(items[0]);
+  var ifState = this.get(items[1]);
+  var elseState = this.get(items[2]);
   return '(' + condition + '?' + ifState + ':' + elseState + ')';
 }
 
-generator.builtins.first = function(items) {
-  var item = generator.get(items[0]);
+Generator.prototype.builtins_first = function(items) {
+  var item = this.get(items[0]);
   return item + '[0]';
 }
 
-generator.builtins.rest = function(items) {
-  var item = generator.get(items[0]);
+Generator.prototype.builtins_rest = function(items) {
+  var item = this.get(items[0]);
   return item + '.slice(1)';
 }
 
-generator.string = function(obj) {
+Generator.prototype.string = function(obj) {
   return '"' + obj.cont + '"';
 }
 
-generator.number = function(obj) {
+Generator.prototype.number = function(obj) {
   return obj.cont;
 }
 
-generator.iden = function(obj) {
+Generator.prototype.iden = function(obj) {
   return obj.cont;
 }
 
-generator.quote = function(obj) {
+Generator.prototype.quote = function(obj) {
   var inner = obj.cont;
   if(inner.type === 'list') {
     var items = [];
     var contents = inner.cont;
     for(var i = 0; i < contents.length; i++) {
       var item = contents[i];
-      items.push(generator.get(item));
+      items.push(this.get(item));
     }
     return '[' + items.join(', ') + ']';
   } else {
-    return generator.get(item);
+    return this.get(item);
   }
 }
 
-generator.list = function(obj) {
-  var funcText = generator.get(obj.cont[0]);
-  if(generator.builtins[funcText]) {
-    return generator.builtins[funcText](obj.cont.slice(1));
-  } else if(generator.infix_operators[funcText]) {
-    return generator.infix(obj);
+Generator.prototype.list = function(obj) {
+  var funcText = this.get(obj.cont[0]);
+  if(this['builtins_' + funcText]) {
+    return this['builtins_' + funcText](obj.cont.slice(1));
+  } else if(this.infix_operators[funcText]) {
+    return this.infix(obj);
   } else {
     if(funcText.startsWith('.')) {
-      var res = generator.get(obj.cont[1]) + funcText + '(';
+      var res = this.get(obj.cont[1]) + funcText + '(';
       var argStart = 2;
     } else if(funcText.startsWith(':')) {
       return this.get(obj.cont[1]) + '.' + funcText.slice(1);
@@ -127,16 +127,17 @@ generator.list = function(obj) {
       var res = funcText + '(';
       var argStart = 1;
     }
-    res += generator.getAll(obj.cont, argStart).join(', ');
+    res += this.getAll(obj.cont, argStart).join(', ');
     res += ')'
     return res;
   }
 }
 
 module.exports.generate = function(tree) {
+  var generator = new Generator();
   var func = generator[tree.type];
   if(!func) {
     throw "Unexpected symbol " + JSON.stringify(tree.cont);
   }
-  return func(tree) + ';';
+  return generator[tree.type](tree) + ';';
 }
