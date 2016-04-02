12c52ca360f08bc82ca5fe477f3363dc89271add
Will Richardson <william.hamish@gmail.com>
2016-04-02 21:43:31 +1300

    Added range command

--- defaults.js
+++ defaults.js
@@ -2,5 +2,7 @@ module.exports = {
   'map': "function map(f, l){var r=[];for(var i=0;i<l.length;i++)r.push(f(l[i]));return r;}",
   'filter': "function filter(f, l){var r=[];for(var i=0;i<l.length;i++)f(l[i])&&r.push(l[i]);return r;}",
   'reduce': "function reduce(f, v, l){for(var i=0;i<l.length;i++)v=f(v,l[i]);return v;}",
-  'type': "function type(a) {return typeof a;}"
+  'type': "function type(a) {return typeof a;}",
+  'range': "function range(a) {var r=[];for(var i=0;i<a;i++)r[i]=i;return r;}",
+  'apply': "function apply(f, a) {return f.apply(null, a);}"
 }
